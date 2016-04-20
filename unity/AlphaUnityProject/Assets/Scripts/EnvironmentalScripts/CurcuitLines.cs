e425edfc657b6c87d16bfb511a105f5bb36e638d
Patrikk Dyrberg Sørensen <pdyr@itu.dk>
2016-04-20 15:43:44 +0200

    Begun working on curcuit lines

---
+++ unity/AlphaUnityProject/Assets/Scripts/EnvironmentalScripts/CurcuitLines.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CurcuitLines : MonoBehaviour {
+
+    public GameObject lineStart;
+    public Transform[] lineDots;
+
+    private LineRenderer lineRenderer;
+    //TODO: Make this be a for each children instead of public assignment
+    void Start()
+    {
+        lineRenderer = lineStart.AddComponent<LineRenderer>();
+        lineRenderer.SetVertexCount(lineDots.Length);
+    }
+    void Update()
+    {
+        DrawLines();
+    }
+
+    void DrawLines()
+    {
+        if (lineDots.Length == 0)
+            return;
+
+        Debug.Log("LineDots length: " + lineDots.Length);
+
+        for(int i = 0; i < lineDots.Length; i++)
+        {
+            Debug.Log("Setting line..." + i + " to position: " + lineDots[i].position);
+            lineRenderer.SetPosition(i, lineDots[i].position); 
+        }
+    }
+}
