ecbb3a57246b6158692b20ba70965b8d29526447
Wintermotes <patri-kk@hotmail.com>
2016-05-03 01:55:30 +0200

    Added maze event, use keykode k

---
+++ unity/AlphaUnityProject/Assets/Scripts/EventScripts/MazeRisingScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening; 
+
+public class MazeRisingScript : MonoBehaviour {
+    public GameObject mazeToMove;
+    public float yPosition = -90.0f;
+    public float duration = 10.0f;
+    public AnimationCurve curve; 
+    public KeyCode keycode;
+
+    private CameraShake camShake; 
+
+    void Start()
+    {
+        camShake = Camera.main.GetComponent<CameraShake>(); 
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(keycode))
+            TriggerScene(); 
+    }
+
+    void TriggerScene()
+    {
+        mazeToMove.transform.DOMoveY(yPosition, duration).SetEase(curve);
+        camShake.Shake();
+    }
+}
