74847b0df8f831e8b61f1dd7b979e6d36f121342
wintermotes <patri-kk@hotmail.com>
2016-05-07 01:09:06 +0200

    Played around with texture animation some more

---
+++ unity/AlphaUnityProject/Assets/Scripts/AnimationScripts/TVScreenBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening; 
+
+public class TVScreenBehaviour : MonoBehaviour {
+
+    public Material offscreenMat; 
+    Renderer r;
+    Material tutScreen, offScreen; 
+
+    void Start()
+    {
+        r = GetComponent<Renderer>();
+        tutScreen = r.material; 
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            r.material = offscreenMat;
+            offscreenMat = r.material; 
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.X))
+            r.material = tutScreen;
+
+        if (Input.GetKeyDown(KeyCode.T))
+            tutScreen.DOOffset(new Vector2(1.0f, 0.0f), 1.0f);
+    }
+
+}
