1488b51f500b367cdc27ef5751b17d7ed2cf8c33
wintermotes <patri-kk@hotmail.com>
2016-05-07 13:01:09 +0200

    Added flickering light, and small prefab cleaning

---
+++ unity/AlphaUnityProject/Assets/Scripts/VFX/FlickeringLight.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening; 
+
+public class FlickeringLight : MonoBehaviour {
+
+    public AnimationCurve lightCurve;
+    public Light lightSource;
+    public float duration, endIntensity = 2.0f; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.X))
+            FlickerLight(); 
+
+	}
+
+    void FlickerLight()
+    {
+        lightSource.DOIntensity(endIntensity, duration).SetEase(lightCurve); 
+    }
+}
