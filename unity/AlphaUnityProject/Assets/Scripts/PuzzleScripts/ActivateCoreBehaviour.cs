acd720697d635f94cacde6eb32956a2de304931d
wintermotes <patri-kk@hotmail.com>
2016-05-08 02:03:31 +0200

    More work on line curcuit puzzles

---
+++ unity/AlphaUnityProject/Assets/Scripts/PuzzleScripts/ActivateCoreBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateCoreBehaviour : MonoBehaviour {
+
+    public GameObject pointEnergySource;
+    
+    void Update()
+    {
+        if (pointEnergySource.GetComponent<Point>().IsDrawn)
+            Debug.Log("I am activated!"); 
+    } 
+}
