0a1cf9efd86673e82a827abf0e85a50fc67499c8
Clarkkentman <jonas.j.jeppesen@gmail.com>
2016-05-13 21:18:23 +0200

    lvl2 fixed wierd obj and added pickup obj with script

---
+++ unity/AlphaUnityProject/Assets/MimiPickUp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MimiPickUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		if(other.gameObject.tag == "BluePickUp")
+		{
+			other.gameObject.SetActive(false);
+		}
+	}
+}
