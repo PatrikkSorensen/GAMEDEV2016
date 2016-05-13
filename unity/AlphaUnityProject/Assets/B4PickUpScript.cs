61eba275537a0dc0148ced36459a3314bccc7c68
Clarkkentman <jonas.j.jeppesen@gmail.com>
2016-05-14 01:14:40 +0200

    lvl1 UI and lvl pick ups

---
+++ unity/AlphaUnityProject/Assets/B4PickUpScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class B4PickUpScript : MonoBehaviour {
+
+	void Start () {
+
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if(other.gameObject.tag == "RedPickUp")
+		{
+			other.gameObject.SetActive(false);
+		}
+	}
+}
---
+++ unity/AlphaUnityProject/Assets/Scripts/MimiPickUp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MimiPickUp : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		if(other.gameObject.tag == "BluePickUp")
+		{
+			other.gameObject.SetActive(false);
+		}
+	}
+}
