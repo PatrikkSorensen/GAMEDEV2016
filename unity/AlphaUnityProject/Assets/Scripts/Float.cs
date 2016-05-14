229551fda393004fc0b2f6821a8a66a31eb5e476
Clarkkentman <jonas.j.jeppesen@gmail.com>
2016-05-14 16:59:02 +0200

    rotate scripte new wind model Lvl1 polished

---
+++ unity/AlphaUnityProject/Assets/Scripts/Float.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Float : MonoBehaviour {
+	public float amplitude;          //Set in Inspector 
+	public float speed;                  //Set in Inspector 
+	private Vector3 moveTo;
+    private Vector3 position;
+
+	void Start () 
+	{
+        position = transform.position;
+    }
+
+	void Update () 
+	{
+        moveTo = transform.position;
+		moveTo.y = position.y + amplitude * Mathf.Sin(speed * Time.time);
+		transform.position = moveTo;
+	}
+
+}
+
