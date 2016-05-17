75ff0a621dee79b2fe63831ddbe58b6721a2ed76
unknown <pdyr@PC608915.itu.local>
2016-05-17 18:19:36 +0200

    Made intro fadein for lvl2

--- unity/AlphaUnityProject/Assets/Scripts/EventScripts/level2/ivl2_introEvent.cs
+++ unity/AlphaUnityProject/Assets/Scripts/EventScripts/level2/ivl2_introEvent.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class ivl2_introEvent : MonoBehaviour {
 
+    public KeyCode debugKey; 
     public AnimationCurve fadeCurve;
     public float fadeTime;
     public GameObject UIImage; 
@@ -12,10 +13,11 @@ public class ivl2_introEvent : MonoBehaviour {
     private GameObject B4, MiMi;
     private Camera cam;
     private Image image;
+    private bool hasPlayed = false; 
 
     void Start()
     {
-        image = UIImage.GetComponent<Image>();
+        image = UIImage.GetComponentInChildren<Image>();
         B4 = GameObject.FindGameObjectWithTag("B4");
         MiMi = GameObject.FindGameObjectWithTag("MiMi");
         cam = Camera.main;
@@ -23,8 +25,20 @@ public class ivl2_introEvent : MonoBehaviour {
         StartCoroutine(createTheBond());
     }
 
+    void Update()
+    {
+        if (Input.GetKey(debugKey) && !hasPlayed)
+        {
+            Debug.Log("Playing lvl 2 intro scene");
+            FadeFromBlack();
+            StartCoroutine(createTheBond());
+            hasPlayed = true; 
+        }
+    }
+
     void FadeFromBlack()
     {
+        Debug.Log(image.name);
         DOTween.To(() => image.color, x => image.color = x, Color.clear, fadeTime).SetEase(fadeCurve);
     }
 
