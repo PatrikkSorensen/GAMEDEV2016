ecc3e7bf06f83ab9d95f7d40e63dd948d94c0906
unknown <jkir@itu.dk>
2016-05-18 15:58:13 +0200

    change speed can affect specific characters

--- unity/AlphaUnityProject/Assets/Scripts/EventScripts/level2/SpeedChangeEventScript.cs
+++ unity/AlphaUnityProject/Assets/Scripts/EventScripts/level2/SpeedChangeEventScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class SpeedChangeEventScript : MonoBehaviour {
 
     public float newSpeed, newFallSpeed, newDashForce, newDrag, newAngularDrag;
+    public bool affectsB4, affectsMiMi;
     private GameObject B4, MiMi;
     private bool b4 = false, mimi = false, done = false;
 
@@ -11,6 +12,14 @@ public class SpeedChangeEventScript : MonoBehaviour {
 	void Start () {
         B4 = GameObject.FindGameObjectWithTag("B4");
         MiMi = GameObject.FindGameObjectWithTag("MiMi");
+        if (!affectsB4)
+        {
+            b4 = true;
+        }
+        if (!affectsMiMi)
+        {
+            mimi = true;
+        }
 	}
 	
 	// Update is called once per frame
@@ -28,21 +37,22 @@ public class SpeedChangeEventScript : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == B4)
+
+        if (other.gameObject == B4 && affectsB4)
         {
             B4.GetComponent<PlayerController>().setSpeed(newSpeed);
             B4.GetComponent<PlayerController>().setUnBoostedSpeed(newSpeed);
+            B4.GetComponent<PlayerController>().setProperties(newFallSpeed, newDashForce, newDrag, newAngularDrag);
             b4 = true;
         }
 
-        if (other.gameObject == MiMi)
+        if (other.gameObject == MiMi && affectsMiMi)
         {
             MiMi.GetComponent<PlayerController>().setSpeed(newSpeed);
             MiMi.GetComponent<PlayerController>().setUnBoostedSpeed(newSpeed);
+            MiMi.GetComponent<PlayerController>().setProperties(newFallSpeed, newDashForce, newDrag, newAngularDrag);
             mimi = true;
         }
-
-        other.gameObject.GetComponent<PlayerController>().setProperties(newFallSpeed, newDashForce, newDrag, newAngularDrag);
 
     }
 }
