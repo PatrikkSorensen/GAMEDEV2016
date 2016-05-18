0514726679418577c30acdb7119c30bdfadc8c9b
unknown <jkir@itu.dk>
2016-05-18 22:23:41 +0200

    Idle loop script added

---
+++ unity/AlphaUnityProject/Assets/Scripts/ObjectScrips/IdleBwobLoopScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IdleBwobLoopScript : MonoBehaviour {
+    public AudioClip loopSound;
+
+    private AudioSource sfxSource;
+	// Use this for initialization
+	void Start () {
+        sfxSource = gameObject.AddComponent<AudioSource>();
+        sfxSource.playOnAwake = false;
+        sfxSource.clip = loopSound;
+        sfxSource.loop = true;
+        sfxSource.spatialBlend = 1.0f;
+        sfxSource.volume = 0.5f;
+        sfxSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
