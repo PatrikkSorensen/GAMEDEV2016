fca843b0a82057c1a3382296b68100c85e64a2d9
Wintermotes <patri-kk@hotmail.com>
2016-05-18 10:16:10 +0200

    More work on ribbon triggers

--- unity/AlphaUnityProject/Assets/Scripts/EventScripts/level2/ribbon/RibbonEndScript.cs
+++ unity/AlphaUnityProject/Assets/Scripts/EventScripts/level2/ribbon/RibbonEndScript.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class RibbonEndScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public AudioClip FadeInMusicClip;
+
+    private bool m_MiMiOnPlatform, m_B4OnPlatform = false;
+    private AudioSource musicController;
+    // Use this for initialization
+    void Start()
+    {
+        musicController = GameObject.FindGameObjectWithTag("MusicController").GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "B4")
+            m_B4OnPlatform = true; ;
+
+        if (coll.gameObject.tag == "MiMi")
+            m_MiMiOnPlatform = true;
+
+        // Fade in music here 
+        if (m_MiMiOnPlatform && m_B4OnPlatform)
+        {
+            musicController.clip = FadeInMusicClip;
+            musicController.Play();
+        }
+
+    }
 }
