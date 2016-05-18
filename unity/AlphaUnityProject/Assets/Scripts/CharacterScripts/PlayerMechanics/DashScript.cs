1e79e17f9c0cb6c7489979c39154572b9e037fcd
unknown <pdyr@PC608915.itu.local>
2016-05-18 13:13:53 +0200

    Merged something

--- unity/AlphaUnityProject/Assets/Scripts/CharacterScripts/PlayerMechanics/DashScript.cs
+++ unity/AlphaUnityProject/Assets/Scripts/CharacterScripts/PlayerMechanics/DashScript.cs
@@ -13,18 +13,8 @@ public class DashScript : MonoBehaviour {
     private float startTime, dashStartTime;
     private float m_rigidbodyDrag;
     private float m_speed;
-<<<<<<< HEAD
-=======
+
     private AudioSource sfxSource;
-
-    void Start()
-    {
-        sfxSource.playOnAwake = false;
-        sfxSource.loop = false;
-        sfxSource.clip = dashSound;
-    }
->>>>>>> 8b01916dfb8c05b4d5fa343b3f59b0eac601893e
-
     private float m_orgSpeed;
     private float m_originalDrag;
 
