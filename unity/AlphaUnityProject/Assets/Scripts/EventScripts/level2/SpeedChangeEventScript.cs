ca1c2a415e62304ed7a4a8a32993a669be23df02
unknown <jkir@itu.dk>
2016-05-18 15:47:36 +0200

    Added speed change event and test scene

--- unity/AlphaUnityProject/Assets/Scripts/EventScripts/level2/SpeedChangeEventScript.cs
+++ unity/AlphaUnityProject/Assets/Scripts/EventScripts/level2/SpeedChangeEventScript.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class SpeedChangeEventScript : MonoBehaviour {
 
-    public float newSpeed;
+    public float newSpeed, newFallSpeed, newDashForce, newDrag, newAngularDrag;
     private GameObject B4, MiMi;
+    private bool b4 = false, mimi = false, done = false;
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +15,15 @@ public class SpeedChangeEventScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if(b4 && mimi)
+        {
+            done = true;
+        }
+
+        if (done)
+        {
+            gameObject.active = false;
+        }
 	}
 
     void OnTriggerEnter(Collider other)
@@ -22,14 +31,18 @@ public class SpeedChangeEventScript : MonoBehaviour {
         if (other.gameObject == B4)
         {
             B4.GetComponent<PlayerController>().setSpeed(newSpeed);
-            B4.GetComponent<PlayerController>().setUnBoostedSpeed(newSpeed);           
+            B4.GetComponent<PlayerController>().setUnBoostedSpeed(newSpeed);
+            b4 = true;
         }
 
         if (other.gameObject == MiMi)
         {
             MiMi.GetComponent<PlayerController>().setSpeed(newSpeed);
-            MiMi.GetComponent<PlayerController>().setUnBoostedSpeed(newSpeed);      
+            MiMi.GetComponent<PlayerController>().setUnBoostedSpeed(newSpeed);
+            mimi = true;
         }
+
+        other.gameObject.GetComponent<PlayerController>().setProperties(newFallSpeed, newDashForce, newDrag, newAngularDrag);
 
     }
 }
